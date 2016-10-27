43b7d4f77d8ffa99285841d27c7d9d25b80dbfd4
Olly.W <yonaichin@gmail.com>
2016-10-27 11:39:34 +0800

    add component Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Hero = (props) => {
+  return (
+    <section className="hero is-medium is-primary is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            威秀影城時刻表
+          </h1>
+          <h2 className="subtitle">
+            Better UX, Better Vieshow. Developed by Naichin.W
+          </h2>
+          <p></p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
+
+
