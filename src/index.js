dddbf04ab3078492843f6f3879a59e5888a435c8
Olly.W <yonaichin@gmail.com>
2016-10-27 11:39:01 +0800

    add bulma framework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import App from './app';
+import 'bulma/css/bulma.css';
 
 ReactDOM.render(
   <App />,
